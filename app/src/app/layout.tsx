a906e3e6cde99e10397bf72f4214708751655616
Julia Astapenko <julia.chvyrova@gmail.com>
2025-02-12 11:51:10 +0300

    Add theme toggle

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -1,7 +1,7 @@
-"use client";
-
 import "./globals.css";
 import SidebarWithState from "@/components/SidebarWithState";
+import { ThemeToggle } from "@/components/ui/theme-toggle";
+import { ThemeProvider } from "@/components/providers/theme-provider"
 
 export default function RootLayout({
   children,
@@ -9,14 +9,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body>
-        <div className="flex h-screen overflow-hidden">
-          <SidebarWithState />
-          <main className="flex-1 overflow-y-auto">
-            {children}
-          </main>
-        </div>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <div className="flex h-screen overflow-hidden">
+            <SidebarWithState />
+            <main className="flex-1 overflow-y-auto">
+              {children}
+            </main>
+          </div>
+          <ThemeToggle className="absolute bottom-2 right-2"/>
+        </ThemeProvider>
       </body>
     </html>
   );
