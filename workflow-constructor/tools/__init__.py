b69adaa96789fb18d0c4b6cd1277dff1d85d3189
wuz1mu <kingaal93@gmail.com>
2025-02-24 22:06:00 +0300

    rm trash

--- workflow-constructor/tools/__init__.py
+++
@@ -1,3 +0,0 @@
-from .agent_tools import get_tools
-
-__all__ = ["get_tools"]
--- workflow-executor/tools/__init__.py
+++
@@ -1,3 +0,0 @@
-from .agent_tools import get_tools, execute_agent_task, evaluate_task_completion
-
-__all__ = ["get_tools", "execute_agent_task", "evaluate_task_completion"]
