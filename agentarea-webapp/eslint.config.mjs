86e27043759a28dbc5106dcac945d46256f323d6
Artem Astapenko <jamakase54@gmail.com>
2025-12-16 22:34:31 +0300

    Update linting command

--- agentarea-webapp/eslint.config.mjs
+++ agentarea-webapp/eslint.config.mjs
@@ -11,7 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   {
-    ignores: [".next/**", "node_modules/**", ".next/**/*", "dist/**"],
+    ignores: [".next/**", "out/**", "build/**", "node_modules/**", "dist/**", "next-env.d.ts"],
   },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   // Global: downgrade exhaustive-deps to warnings to avoid CI failures
