f1f4b578a668099c85d74e08cfab8c88971a4de7
Artem Astapenko <jamakase54@gmail.com>
2026-06-08 01:52:10 +0300

    chore(alembic): merge bundles + resource-provenance heads

---
+++ agentarea-platform/apps/api/alembic/versions/20260608_0151_merge_bundles_and_resource_provenance_.py
@@ -0,0 +1,28 @@
+"""merge bundles and resource provenance heads
+
+Revision ID: 20260607_0001_merge_heads
+Revises: 20260603_installed_bundles, 20260606_0900_agent_reg_prov
+Create Date: 2026-06-08 01:51:56.045470
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '20260607_0001_merge_heads'
+down_revision: Union[str, None] = ('20260603_installed_bundles', '20260606_0900_agent_reg_prov')
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
