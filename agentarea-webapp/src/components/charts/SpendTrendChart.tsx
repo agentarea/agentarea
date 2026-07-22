3bca782983c9e613069cfd70a8983514a92a456c
Julia Astapenko <julia.chvyrova@gmail.com>
2026-07-22 16:08:00 +0300

    fix

--- agentarea-webapp/src/components/charts/SpendTrendChart.tsx
+++ agentarea-webapp/src/components/charts/SpendTrendChart.tsx
@@ -45,7 +45,7 @@ export function SpendTrendChart({
   cumulativeLabel = "cumulative",
 }: {
   data: Point[];
-  height?: number | string;
+  height?: number | `${number}%`;
   locale?: string;
   seriesLabel?: string;
   cumulativeLabel?: string;
